23d12b3287804aa17812b3cf6141bd1c31927218
sendmithunb@gmail.com <sendmithunb@gmail.com>
2021-04-03 14:04:05 -0500

    add chakraui and landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
 // eslint-disable-next-line
-import { Heading } from '@chakra-ui/react';
+import { Heading, Image, Flex, Center, Box, IconButton, Text, Button, Container } from '@chakra-ui/react';
+// import { HamburgerIcon } from '@chakra-ui/icons';
+// import logo from "./default.png";
+import banner from "./profile.png"
 
 function App() {
   return (
-    <div>
-      <header>
-        <Heading>Clip</Heading>
-        <img src="default.png"></img>
+    <div id="main">
+      {
+        /* 
+        <header>
+          <Box bgColor="#fffffc">
+            <Center>
+              <Image src={banner} alt="Clip! logo" boxSize="100px" objectFit="cover"></Image>
+            </Center>
+        </Box>
       </header>
+      */
+      }
+
+      <Flex justify="center" minHeight="100vh" alignItems="center">
+        <Box>
+          <Image src={banner} alt="Clip! logo" height="auto" width="40rem"></Image> 
+        </Box>
+        <Box>
+          <Text fontSize="5xl">Bringing <strong>community</strong> back into <strong>online learning.</strong></Text>
+          <Center>
+            <Button bgColor="#2ec4b6" marginTop="2rem">Get Started</Button>
+          </Center>
+        </Box>
+      </Flex>
+
     </div>
   );
 }
